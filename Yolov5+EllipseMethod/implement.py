4a68c55a9c24a7c20d6fa52630dd566f574d4924
Summer <71572746+SummerSigh@users.noreply.github.com>
2022-03-16 23:10:12 -0700

    Retrained model with lower loss and lowered implement.py conf thresh
     Run summary:
                 best/epoch 15
               best/mAP_0.5 0.8845
          best/mAP_0.5:0.95 0.37727
             best/precision 0.95714
                best/recall 0.80715
            metrics/mAP_0.5 0.88475
       metrics/mAP_0.5:0.95 0.3772
          metrics/precision 0.95714
             metrics/recall 0.80716
             train/box_loss 0.01551
             train/cls_loss 0.0
             train/obj_loss 0.0045
               val/box_loss 0.04536
               val/cls_loss 0.0
               val/obj_loss 0.00589
                      x/lr0 0.00624
                      x/lr1 0.00624
                      x/lr2 0.00624

--- Yolov5+EllipseMethod/implement.py
+++ Yolov5+EllipseMethod/implement.py
@@ -2,7 +2,7 @@ import torch
 import cv2
 # loads a video file and returns bounding box detections
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt',force_reload=True)
-model.conf = 0.25  # NMS confidence threshold
+model.conf = 0.1  # NMS confidence threshold
 model.iou = 0.45  # NMS IoU threshold
 model.agnostic = False  # NMS class-agnostic
 model.multi_label = False  # NMS multiple labels per box
@@ -13,14 +13,17 @@ img = cv2.VideoCapture("Test.mp4") #Change the path to your video file
 while(img.isOpened()):
     ret, frame = img.read() # read a frame
     results = model(frame)  # inference
-    for box in results.xyxy[0]:   # box is a list of 4 numbers
-        if box[5]==0:       # if the confidence is 0, then skip       
-            xB = int(box[2])   # xB is the x coordinate of the bottom right corner
-            xA = int(box[0])   # xA is the x coordinate of the top left corner
-            yB = int(box[3])   # yB is the y coordinate of the bottom right corner
-            yA = int(box[1])   # yA is the y coordinate of the top left corner
-            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2) # draw a rectangle around the detected object
-            cv2.circle(frame, (int((xA+xB)/2), int((yA+yB)/2)), 2, (0, 0, 255), -1)
+    try:
+        for box in results.xyxy[0]:   # box is a list of 4 numbers
+            if box[5]==0:       # if the confidence is 0, then skip       
+                xB = int(box[2])   # xB is the x coordinate of the bottom right corner
+                xA = int(box[0])   # xA is the x coordinate of the top left corner
+                yB = int(box[3])   # yB is the y coordinate of the bottom right corner
+                yA = int(box[1])   # yA is the y coordinate of the top left corner
+                cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2) # draw a rectangle around the detected object
+                cv2.circle(frame, (int((xA+xB)/2), int((yA+yB)/2)), 2, (0, 0, 255), -1)
+    except:
+        pass
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
